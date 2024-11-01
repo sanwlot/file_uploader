55ab63190a2e702643d3bbf74f37b1eef274e347
sunnySavlot <sunny.savlot@gmail.com>
2024-11-01 09:11:06 +0530

    redudendant code removed

--- app.js
+++ app.js
@@ -13,12 +13,6 @@ dotenv.config()
 
 const app = express()
 
-// import path from "path"
-// import { fileURLToPath } from "url"
-// const __filename = fileURLToPath(import.meta.url)
-// const __dirname = path.dirname(__filename)
-// app.set("views", path.join(__dirname, "views"))
-
 app.set("view engine", "ejs")
 app.use(express.static("public"))
 app.use(express.json())
